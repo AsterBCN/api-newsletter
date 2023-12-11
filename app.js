16b3b3171bf603f596c0fec9d2b752f5a4395633
AsterBCN <despatx4@aster.cat>
2023-12-11 17:39:24 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+
+import express from 'express'
+import {getUrlImageDrive} from './getImages.js'
+
+const app = express();
+
+// Define a route that includes a parameter in the URL
+// https://drive.google.com/file/d/1TA7hHk_RyP74HB-7harW_nEuYw9cmDlK/view?usp=sharing
+await app.get('/drive/:name', async (req, res) =>  {
+  // Retrieve the parameter from the URL
+  
+  const name = req.params.name;
+  console.log(name)
+  const url = await getUrlImageDrive(name)
+
+  // Send a personalized greeting back to the client
+  res.send(`URL: ${url} `);
+});
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
